69072e6820487ee3bc0f42e07500cb6616441495
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-06 11:11:37 +0530

    add factorial method

--- src/main/Factorial.java
+++ src/main/Factorial.java
@@ -1,5 +1,21 @@
 package main;
 
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.math.BigInteger;
+
 public class Factorial {
+   static BigInteger factorial(BigInteger number)
+   {
+       if (number.equals(BigInteger.ONE))
+       {
+           return BigInteger.ONE;
+       }
+       return number.multiply(factorial(number.subtract(BigInteger.ONE)));
+   }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(BigInteger.valueOf(15)));
+    }
 
 }
