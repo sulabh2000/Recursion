d9457b66acc1c04c67f0ffac95c651828d9ddd45
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-06 10:50:19 +0530

    modifications

--- src/main/Recursion.java
+++ src/main/Recursion.java
@@ -1,5 +1,21 @@
 package main;
 
 public class Recursion {
+    static void countDown(int number)
+    {
+        if(number<=0)
+        {
+            return;     // ---> break cannot be used as it can only be used in loops.
+        }
+        System.out.println(number);
+        --number;
+        countDown(number);
+    }
 
+    public static void main(String[] args) {
+        /*Recursion recursion = new Recursion();               --> if not using static with method
+        recursion.countDown(4);*/
+        countDown(4);
+
+    }
 }
