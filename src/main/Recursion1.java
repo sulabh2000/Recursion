e86bce3d4daca5f206843d27116ca44162cad015
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-06 10:59:11 +0530

    add method

--- src/main/Recursion1.java
+++ src/main/Recursion1.java
@@ -1,5 +1,16 @@
 package main;
 
 public class Recursion1 {
+        static int sum(int number)
+        {
+            if(number == 1)
+            {
+                return 1;
+            }
+            return number + sum(--number);
+        }
 
+    public static void main(String[] args) {
+        sum(4);
+    }
 }
